423016f7d6d34565dba9fcfa6802831a1dde9cb7
Sufi K Sulaiman <ssufikhan@icloud.com>
2025-11-29 10:37:37 +0000

    File changes

--- src/pages/Learning.jsx
+++ src/pages/Learning.jsx
@@ -348,7 +348,7 @@ export default function Learning() {
             </div>
 
             {/* Learning Islands Grid */}
-            <div className="max-w-7xl mx-auto px-4 md:px-8 pb-12">
+            <div className="max-w-7xl mx-auto px-4 md:px-8 pt-8 pb-12">
                 {loadingTopics ? (
                     <div className="flex flex-col items-center justify-center py-20">
                         <style>{`
