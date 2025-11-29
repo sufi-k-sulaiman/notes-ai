1012079d1c7fa3bcea849bd94a493362b7255bc6
Sufi K Sulaiman <ssufikhan@icloud.com>
2025-11-29 11:04:31 +0000

    File changes

--- src/pages/Learning.jsx
+++ src/pages/Learning.jsx
@@ -270,7 +270,7 @@ export default function Learning() {
                 {/* Subject Selector - Right */}
                 <div className="bg-white rounded-2xl shadow-sm border border-gray-100 p-4 lg:w-1/2 flex flex-col">
                     {/* Search Bar */}
-                    <div className="mb-4">
+                    <div className="mb-2">
                         <div className="relative">
                             <Search className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
                             <input
