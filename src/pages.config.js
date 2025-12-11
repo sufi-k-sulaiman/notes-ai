990f6aa7ca2de8f565ffc3419345d3bc9b45e8d9
Sufi K Sulaiman <ssufikhan@icloud.com>
2025-12-11 05:07:24 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,29 +1,13 @@
-import TestFunctions from './pages/TestFunctions';
-import DashboardComponents from './pages/DashboardComponents';
-import Home from './pages/Home';
-import Template from './pages/Template';
-import SearchResults from './pages/SearchResults';
 import Notes from './pages/Notes';
-import Comms from './pages/Comms';
-import Search from './pages/Search';
-import NotFound from './pages/NotFound';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "TestFunctions": TestFunctions,
-    "DashboardComponents": DashboardComponents,
-    "Home": Home,
-    "Template": Template,
-    "SearchResults": SearchResults,
     "Notes": Notes,
-    "Comms": Comms,
-    "Search": Search,
-    "NotFound": NotFound,
 }
 
 export const pagesConfig = {
-    mainPage: "TestFunctions",
+    mainPage: "Notes",
     Pages: PAGES,
     Layout: __Layout,
 };
